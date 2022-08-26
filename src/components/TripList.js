0c8b7de1f915322de2d2d4aca88a654a81f1c73d
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-08-26 14:46:46 +0900

    8/26

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,15 +1,37 @@
 import { useState } from "react";
+import useFetch from "../hooks/useFetch";
+
+import "./TripList.css";
 
 export default function TripList() {
-  //   const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
 
-  //   fetch("http://localhost:3000/trips")
-  //     .then((response) => response.json())
-  //     .then((json) => setTrips(json));
+  const { data: trips, isPending, error } = useFetch(url);
 
   return (
-    <div>
+    <div className="trip-list">
       <h2>Trip List</h2>
+      {isPending && <div>Loading, please wait</div>}
+      {error && <div>{error}</div>}
+      <ul>
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
+      </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 }
